e304748b01cdcc33f36d69ee290dabfe68b0c8fa
Glen <iepathos@gmail.com>
2013-08-27 02:42:25 -0700

    Added login and logout separate from admin interface

--- dreamworld/settings.py
+++ dreamworld/settings.py
@@ -31,34 +31,17 @@ ALLOWED_HOSTS = ['*']
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
-# Local time zone for this installation. Choices can be found here:
-# http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
-# although not all choices may be available on all operating systems.
-# In a Windows environment this must be set to your system time zone.
 TIME_ZONE = 'America/Los_Angeles'
-
-# Language code for this installation. All choices can be found here:
-# http://www.i18nguy.com/unicode/language-identifiers.html
 LANGUAGE_CODE = 'en-us'
-
 SITE_ID = 1
-
-# If you set this to False, Django will make some optimizations so as not
-# to load the internationalization machinery.
 USE_I18N = True
-
-# If you set this to False, Django will not format dates, numbers and
-# calendars according to the current locale.
 USE_L10N = True
-
-# If you set this to False, Django will not use timezone-aware datetimes.
 USE_TZ = True
 
 MEDIA_ROOT = ''
 MEDIA_URL = '/media/'
 
 # Static asset configuration
-
 STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
 
@@ -112,11 +95,14 @@ INSTALLED_APPS = (
     'django.contrib.staticfiles',
     'django.contrib.admin',
     'south',
-
+    'core',
     'cities',
 )
 
-#LOGIN_URL = '/admin'
+LOGIN_URL = '/login/'
+LOGOUT_URL ='/logout/'
+LOGIN_REDIRECT_URL = '/'
+
 
 # A sample logging configuration. The only tangible logging
 # performed by this configuration is to send an email to
--- dreamworld/urls.py
+++ dreamworld/urls.py
@@ -2,6 +2,8 @@ from django.conf.urls import patterns, include, url
 
 from django.views.generic import TemplateView
 
+from core.views import logout
+
 from django.contrib import admin
 admin.autodiscover()
 
@@ -9,4 +11,8 @@ urlpatterns = patterns('',
 	url(r"^$", TemplateView.as_view(template_name="homepage.html"), name="home"),
 	url(r'^cities/', include('cities.urls')),
     url(r'^admin/', include(admin.site.urls)),
+
+    # Login / Logout
+    (r'^login/$', 'django.contrib.auth.views.login'),
+    (r'^logout/$', 'django.contrib.auth.views.logout'),
 )
